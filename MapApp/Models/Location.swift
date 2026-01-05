f274a1d1a56ce4465098be90acad12b8ba4bebb7
Sukhpreet Singh <ss0218308@gmail.com>
2026-01-05 06:11:17 +0530

    add location data service class and location model files to the project

---
+++ MapApp/Models/Location.swift
@@ -0,0 +1,18 @@
+//
+//  Location.swift
+//  MapApp
+//
+//  Created by Sukhpreet Singh on 03/01/26.
+//
+
+import Foundation
+import MapKit
+
+struct Location {
+    let name: String
+    let cityName: String
+    let coordinates: CLLocationCoordinate2D
+    let description: String
+    let imageNames: [String]
+    let link: String
+}
